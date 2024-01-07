95872de8be83ab52b25028b18401ec481c5edcfa
Michal <michalekb8@gmail.com>
2024-01-07 23:06:35 +0100

    Enemy Movement

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    GameObject player;
+    Rigidbody rb;
+    public float speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        rb.AddForce(direction * speed);
+    }
+}
