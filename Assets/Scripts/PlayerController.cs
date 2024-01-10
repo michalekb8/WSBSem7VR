b1f2c993511b1cffff8d487e607d6c56122f9f2d
Michal <michalekb8@gmail.com>
2024-01-10 20:09:13 +0100

    Audio effects on collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,14 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 5.0f;
 
+    AudioSource collisionAudio;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
+        collisionAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,5 +24,13 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collisionAudio.Play();
+        }
     }
 }
